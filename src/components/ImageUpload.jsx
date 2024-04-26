15d3c4df24e21587506468b21dbbb2f76eedb267
ayushp2563 <ayushprajapati2563@gmail.com>
2024-04-26 18:13:21 +0530

    minor change

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { IoCloudUploadOutline } from "react-icons/io5";
 
@@ -6,7 +6,6 @@ const ImageUpload = () => {
   const [file, setFile] = useState(null);
   const [imageUrl, setImageUrl] = useState("");
   const [copyStatus, setCopyStatus] = useState("Copy URL");
-  const [error, setError] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -31,16 +30,9 @@ const ImageUpload = () => {
           },
         }
       );
-
       setImageUrl(response.data.url);
       setCopyStatus("Copy URL");
-      setError(null); // Reset error state
     } catch (error) {
-      if (error.code === "ERR_NETWORK") {
-        setError("Network error. Please check your internet connection.");
-      } else {
-        setError("Error uploading image: " + error.message);
-      }
       console.error("Error uploading image:", error);
     }
   };
@@ -70,18 +62,24 @@ const ImageUpload = () => {
         <input
           type="file"
           onChange={handleFileChange}
-          className="bg-black px-4 py-4 rounded-2xl block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+          className="bg-black px-4 py-4 rounded-2xl block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-full file:border-0
+      file:text-sm file:font-semibold
+      file:bg-violet-50 file:text-violet-700
+      hover:file:bg-violet-100"
         />
       </div>
+
       <button
         onClick={handleUpload}
         className="bg-blue-500 text-white py-2 px-4 rounded-2xl hover:bg-blue-600 transition-colors duration-200"
       >
         <p className="flex flex-row items-center justify-center">
-          <IoCloudUploadOutline className="mr-2" /> Upload Image
+          <IoCloudUploadOutline className="mr-2" />
+          Upload Image
         </p>
       </button>
-      {error && <p className="text-red-500">{error}</p>}
       {imageUrl && (
         <div className="flex flex-col items-center space-x-2 py-2">
           <img
