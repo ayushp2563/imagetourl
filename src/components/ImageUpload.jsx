abb8df5ac91ed1e4d3c596d5a88b67045f377be6
ayushp2563 <ayushprajapati2563@gmail.com>
2024-04-30 22:42:30 +0530

    minor change

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -58,49 +58,58 @@ const ImageUpload = () => {
   };
 
   return (
-    <div className="flex flex-col items-center space-y-4 rounded-2xl">
-      <div>
-        <input
-          type="file"
-          onChange={handleFileChange}
-          className="bg-black px-4 py-4 rounded-2xl block w-full text-sm text-slate-500
+    <div className="min-h-screen flex justify-center items-center bg-slate-800 px-4 py-4 ">
+      <div className="flex items-center flex-col justify-center bg-white p-8 rounded-2xl shadow-lg">
+        {/* <img className="p-4 w-60 h-66" src={logo} /> */}
+        <h1 className="jersey-20-regular">Image to URL</h1>
+        <div className="flex flex-col items-center space-y-4 rounded-2xl">
+          <div>
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="bg-black px-4 py-4 rounded-2xl block w-full text-sm text-slate-500
       file:mr-4 file:py-2 file:px-4
       file:rounded-full file:border-0
       file:text-sm file:font-semibold
       file:bg-violet-50 file:text-violet-700
       hover:file:bg-violet-100"
-        />
-      </div>
-
-      <button
-        onClick={handleUpload}
-        className="bg-blue-500 text-white py-2 px-4 rounded-2xl hover:bg-blue-600 transition-colors duration-200"
-      >
-        <p className="flex flex-row items-center justify-center">
-          <IoCloudUploadOutline className="mr-2" />
-          Upload Image
-        </p>
-      </button>
-      {imageUrl && (
-        <div className="flex flex-col items-center space-x-2 py-2">
-          <img
-            src={imageUrl}
-            alt="Uploaded"
-            className="w-auto h-auto rounded-md px-4"
-          />
-          <div className="flex flex-col items-center space-x-2 py-2">
-            <p className="break-all text-sm bg-slate-400 rounded-2xl p-4 mb-2">
-              {imageUrl}
-            </p>
-            <button
-              onClick={copyToClipboard}
-              className="bg-blue-500 text-white py-2 px-4 rounded-2xl hover:bg-blue-600 transition-colors duration-200"
-            >
-              {copyStatus}
-            </button>
+            />
           </div>
+
+          <button
+            onClick={handleUpload}
+            className="bg-blue-500 text-white py-2 px-4 rounded-2xl hover:bg-blue-600 transition-colors duration-200"
+          >
+            <p className="flex flex-row items-center justify-center">
+              <IoCloudUploadOutline className="mr-2" />
+              Upload Image
+            </p>
+          </button>
+          {imageUrl && (
+            <div className="flex flex-col items-center space-x-2 py-2">
+              <img
+                src={imageUrl}
+                alt="Uploaded"
+                className="w-auto h-auto rounded-md px-4"
+              />
+              <div className="flex flex-col items-center space-x-2 py-2">
+                <p className="break-all text-sm bg-slate-400 rounded-2xl p-4 mb-2">
+                  {imageUrl}
+                </p>
+                <button
+                  onClick={copyToClipboard}
+                  className="bg-blue-500 text-white py-2 px-4 rounded-2xl hover:bg-blue-600 transition-colors duration-200"
+                >
+                  {copyStatus}
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-      )}
+        <div className="text-black flex items-center justify-center mt-4">
+          Made with 💛 by Ayush Prajapati
+        </div>
+      </div>
     </div>
   );
 };
